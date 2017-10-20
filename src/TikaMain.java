11f933ffc82292d275b401f7d91cb0325ebd118a
Juanjo Sierra <jjsierra103@gmail.com>
2017-10-20 09:53:20 +0200

    Erased some commented code and Tika object.

--- src/TikaMain.java
+++ src/TikaMain.java
@@ -15,7 +15,6 @@ public class TikaMain {
 
     public static void main(String [] args) throws Exception{
 
-        Tika tika = new Tika();
         // Se parsean los ficheros pasados como argumento y se extrae el contenido
 
         String directorioDatos = System.getProperty("user.dir") + "/datosEntrada/";
@@ -43,36 +42,7 @@ public class TikaMain {
                 try {
 
                     metadatas.extractionMetadata(flujo, flujo2);
-
-                    /*Metadata metadata = new Metadata();
-
-                    //Le pasamos el -1 para que no nos salte un error de que el fichero sobrepasa el límite de caracteres.
-                    ContentHandler handler = new BodyContentHandler(-1);
-
-                    ParseContext parseContext = new ParseContext();
-
-                    AutoDetectParser parser = new AutoDetectParser();
-
-                    parser.parse(flujo, handler, metadata, parseContext);
-
-                    Occurrences occurr = new Occurrences(handler);
-
-                    occurr.occurrencesToFile(metadata.get("title"));
-
-                    LanguageDetector identifier = new OptimaizeLangDetector().loadModels();
-
-                    LanguageResult idioma = identifier.detect (handler.toString());
-
-                    System.out.println("-----------------------------------------------------------------------------");
-
-                    //Ejemplo de sacar metadatos por su nombre.
-                    System.out.println("Title: " + metadata.get("title"));
-                    System.out.println("Format: " + metadata.get(Metadata.CONTENT_TYPE));
-                    System.out.println("Conding: " + metadata.get(Metadata.CONTENT_ENCODING));
-                    System.out.println("Language " + metadata.get("language"));
-                    System.out.println("Language Detect: " + idioma.getLanguage());
-
-                    System.out.println("-----------------------------------------------------------------------------");*/
+                    
 
                 } finally {
 
